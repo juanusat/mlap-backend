6a5800d04455497056d520922bbcded71b3bf093
juanusat <71309030@usat.pe>
2025-11-13 08:08:51 -0500

    eliminar prueba innecesaria

--- test_controller.js
+++
@@ -1,6 +0,0 @@
-const scheduleController = require('./controllers/scheduleController');
-
-console.log('scheduleController:', scheduleController);
-console.log('Type:', typeof scheduleController);
-console.log('listGeneralSchedules:', typeof scheduleController.listGeneralSchedules);
-console.log('Keys:', Object.keys(scheduleController));
